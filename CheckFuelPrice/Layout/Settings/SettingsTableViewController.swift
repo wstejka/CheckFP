a9111066b15e0f288a7d260ec6e85abd18c73aa7
Wojciech S <wstejka@gmail.com>
2017-07-01 22:28:25 +0200

    BG-52: Initial work for Settings section; part 2

--- CheckFuelPrice/Layout/Settings/SettingsTableViewController.swift
+++ CheckFuelPrice/Layout/Settings/SettingsTableViewController.swift
@@ -18,32 +18,115 @@ extension SettingsTableViewController {
 class SettingsTableViewController: UITableViewController {
 
     
-    // MARK: - Property
+    // MARK: - Properties/Outlets
     
     // MARK: - Section: themes
-    @IBOutlet weak var themeColorPrimaryCell: UITableViewCell!
+    @IBOutlet weak var theme: UILabel!
     
     // MARK: - Section: fuel price settings
-    @IBOutlet weak var producersCell: UITableViewCell!
-    
-    @IBOutlet weak var vatCell: UITableViewCell!
-    @IBOutlet weak var taxAmountCell: UITableViewCell!
-    @IBOutlet weak var priceUnitOfMeasureCell: UITableViewCell!
+    // producer
+    @IBOutlet weak var producerLabel: UILabel!
+    // VAT
+    @IBOutlet weak var vatLabel: UILabel!
+    // Tax amount
+    @IBOutlet weak var taxAmountLabel: UILabel!
+    @IBOutlet weak var taxAmountValueLabel: UILabel!
+    // price unit
+    @IBOutlet weak var priceUnitLabel: UILabel!
     
     // MARK: - Section: Profit margin per fuel type
     
-    @IBOutlet weak var unleaded95Cell: UITableViewCell!
-    @IBOutlet weak var unleaded98Cell: UITableViewCell!
-    @IBOutlet weak var dieselCell: UITableViewCell!
-    @IBOutlet weak var dieselPremiumCell: UITableViewCell!
-    @IBOutlet weak var dieselHeatingCell: UITableViewCell!
+    @IBOutlet weak var unleaded95Label: UILabel!
+    @IBOutlet weak var unleaded95ValueLabel: UILabel!
+    
+    @IBOutlet weak var unleaded98Label: UILabel!
+    @IBOutlet weak var unleaded98ValueLabel: UILabel!
+    
+    @IBOutlet weak var dieselLabel: UILabel!
+    @IBOutlet weak var dieselValueLabel: UILabel!
+    
+    @IBOutlet weak var dieselPremiumLabel: UILabel!
+    @IBOutlet weak var dieselPremiumValueLabel: UILabel!
+    
+    @IBOutlet weak var dieselHeatingLabel: UILabel!
+    @IBOutlet weak var dieselHeatingValueLabel: UILabel!
     
     // MARK: - TableViewController lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        initialConfiguration()
     }
     
     
+    // MARK: - Actions
+    // MARK: - Section: fuel price settings
+    @IBAction func producerButtonPressed(_ sender: UIButton) {
+        log.verbose("")
+    }
+    
+    @IBAction func vatOptionSelected(_ sender: UISegmentedControl) {
+        log.verbose("")
+    }
+    
+    @IBAction func taxAmountValueChanged(_ sender: UISlider) {
+        log.verbose("")
+    }
 
+    @IBAction func priceUnitValueChanged(_ sender: UISegmentedControl) {
+        log.verbose("")
+    }
+
+    // MARK: - Section: Profit margin per fuel type
+    
+    @IBAction func unleaded95ValueChanged(_ sender: UISlider) {
+        log.verbose("")
+        unleaded95ValueLabel.text = getFuelLabelTextFor(sender.value)
+    }
+    
+    @IBAction func unleaded98ValueChanged(_ sender: UISlider) {
+        log.verbose("")
+        unleaded98ValueLabel.text = getFuelLabelTextFor(sender.value)
+    }
+
+    @IBAction func dieselValueChanged(_ sender: UISlider) {
+        log.verbose("")
+        dieselValueLabel.text = getFuelLabelTextFor(sender.value)
+    }
+
+    @IBAction func dieselPremiumValueChanged(_ sender: UISlider) {
+        log.verbose("")
+        dieselPremiumValueLabel.text = getFuelLabelTextFor(sender.value)
+    }
+
+    @IBAction func dieselHeatingValueChanged(_ sender: UISlider) {
+        log.verbose("")
+        dieselHeatingValueLabel.text = getFuelLabelTextFor(sender.value)
+    }
+
+    
+    
+    // MARK: - Methods
+    
+    func getFuelLabelTextFor(_ value : Float) -> String {
+        return String(format: "%.1f", value) + " %"
+    }
+    
+    func initialConfiguration() {
+        log.verbose("")
+
+        producerLabel.text = "fuelSupplier".localized().capitalizingFirstLetter()
+        vatLabel.text = "includeVatInFuelPrice".localized().capitalizingFirstLetter()
+        taxAmountLabel.text = "vatTaxAmount".localized().capitalizingFirstLetter()
+        priceUnitLabel.text = "capacity".localized().capitalizingFirstLetter()
+
+        unleaded95Label.text = "unleaded95".localized().capitalizingFirstLetter()
+        unleaded98Label.text = "unleaded98".localized().capitalizingFirstLetter()
+        dieselLabel.text = "diesel".localized().capitalizingFirstLetter()
+        dieselPremiumLabel.text = "dieselIZ40".localized().capitalizingFirstLetter()
+        dieselHeatingLabel.text = "dieselHeating".localized().capitalizingFirstLetter()
+                
+        
+    }
+    
 }
