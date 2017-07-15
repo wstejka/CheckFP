c4ac2204e50687e6a7745039d14f7c78250ae25d
Wojciech S <wstejka@gmail.com>
2017-07-15 20:26:57 +0200

    BG-73: Implement Edit/Add actions on Purchase View

--- CheckFuelPrice/Layout/Orders/PurchaseUpdateViewController.swift
+++ CheckFuelPrice/Layout/Orders/PurchaseUpdateViewController.swift
@@ -13,6 +13,30 @@ protocol PurchaseUpdateViewControllerDelegate {
     func savedPurchase(snapshot: DataSnapshot)
 }
 
+extension PurchaseUpdateViewController: UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataPickerItems.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataPickerItems[row].getLocalizedText()
+    }
+
+}
+
+extension PurchaseUpdateViewController : UIPickerViewDelegate {
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        log.verbose("Currently picked: \(dataPickerItems[row].getLocalizedText() ?? "")")
+    }
+}
+
+
 class PurchaseUpdateViewController: UIViewController {
 
     // MARK: - Outlets
@@ -20,17 +44,67 @@ class PurchaseUpdateViewController: UIViewController {
     @IBOutlet weak var leftButtonItem: UIBarButtonItem!
     @IBOutlet weak var rightButtonItem: UIBarButtonItem!
     
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var actionTextLabel: UILabel!
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var priceTextField: UITextField!
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    
     // MARK: - Vars/Consts
     public let delegate : PurchaseUpdateViewControllerDelegate? = nil
     
     var snapshot : FuelPurchase? = nil
+    
+    let dataPickerItems : [Fuel] = {
+       
+        var items : [Fuel] = []
+        for fuelType in iterateEnum(Fuel.self) {
+            if fuelType == .none {
+                continue
+            }
+            items.append(fuelType)
+        }
+        
+        return items
+    }()
     
     // MARK: - UIViewController lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-
+        if snapshot == nil {
+            actionTextLabel.text = "addNewPurchase".localized()
+            snapshot = FuelPurchase()
+            snapshot?.fuelType = Fuel.unleaded95.rawValue
+        }
+        else {
+            actionTextLabel.text = "editExistingPurchase".localized()
+        }
+        guard let fuelPurchase = snapshot else { return }
+        
+        // set up current values
+        pickerView.selectRow(fuelPurchase.fuelType - 1, inComponent: 0, animated: true)
+        datePicker.setDate(Date(timeIntervalSince1970: Double(fuelPurchase.timestamp)), animated: true)
+        
+        amountLabel.text = "amount".localized()
+        amountTextField.text = fuelPurchase.amount.strRound(to: 2)
+        
+        priceLabel.text = "price".localized()
+        priceTextField.text = fuelPurchase.price.strRound(to: 2)
+        
+        // set up numeric keyboard to simplify providing data
+        amountTextField.keyboardType = UIKeyboardType.numbersAndPunctuation
+        priceTextField.keyboardType = UIKeyboardType.numbersAndPunctuation
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,6 +112,12 @@ class PurchaseUpdateViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        
+        return UIInterfaceOrientationMask.portrait
+    }
 
     // MARK: - Actions
     
@@ -48,5 +128,10 @@ class PurchaseUpdateViewController: UIViewController {
 
     @IBAction func rightButtonItemPressed(_ sender: UIBarButtonItem) {
     }
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        log.verbose("\(sender.date)")
+    }
+    
     
 }
